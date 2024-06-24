4f293f4b361e31712e5764bd79a4d2d4417a7f7a
Vera-Felix <verafelix37@gmail.com>
2024-06-24 20:56:34 -0300

    Estudos e exercicios

---
+++ exercicios-sala/parimpar.py
@@ -0,0 +1,11 @@
+# Exercicio par ou impar, teremos uma lista com alguns números e vamos dividir em duas listas 
+# uma com números pares e outra com números impares.
+
+#Faça um Programa que leia 10 números inteiros e armazene-os num vetor. 
+#Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+
+def main():
+    numeros = input("Digite 10 números separados por vírgula: ")
+    
+
+main()
---
+++ exercicios-sala/quadrado.py
@@ -0,0 +1,12 @@
+# Exercicio quadrado, teremos uma lista com alguns números e vamos gerar uma nova lista com o 
+# quadrado desses números.
+
+# Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos
+# quadrados dos elementos do vetor.
+
+
+def main():
+    numeros = input("Digite 10 números separados por vírgula: ")
+
+
+main()
